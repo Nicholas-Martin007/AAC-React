0dd5c7681fb17e334ff3073962981ee5de605f67
Nicholas Martin <nicholas.martin03@yahoo.com>
2025-09-25 16:25:11 +0700

    Add Basic Structure

---
+++ src/Pages/Home/HomeMain.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function HomeMain() {
+    return (
+        <Box width={"100%"} height={"100%"}>
+            <Box position={"sticky"}>
+                <Text>test</Text>
+            </Box>
+        </Box>
+    )
+}
+
+
+//https://fonts.google.com/specimen/Mona+Sans
